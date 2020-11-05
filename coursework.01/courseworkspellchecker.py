ecdc0cbde6340259f74e4a3a3f03506369d14668
u17296as <ali.sarwar@student.manchester.ac.uk>
2020-11-05 15:19:08 +0000

    Now reads words in from dictionary file

--- coursework.01/courseworkspellchecker.py
+++ coursework.01/courseworkspellchecker.py
@@ -1,51 +1,48 @@
 import os, time, os.path
-def intValidator(choice):
-    try:
-        choice = int(choice)
-    except:
-        print("Invalid input entered.")
-    return choice
+
+dictList = []
+with open("englishwords.txt", 'r') as file:
+    for line in file:
+        dictList.append(line.strip("\n"))
     
-def sentenceCheck():
-    checkSentence = input("\nPlease enter the sentence you wish to check : ")
-    sentenceList = []
-    sentenceList = checkSentence
-    wordOutput = sentenceList.split(" ")
-    print(wordOutput)
-    return(checkSentence)
 
-def fileCheck():
-    fNameCheck = input("\nPlease enter the filename that you would like to check, along with the .txt extension : ")
-    try:
-        fileList = []
-        with open(fNameCheck, 'r') as file:
-            fileList = file.read()
-            wordOutput = fileList.split(" ")
-            print(wordOutput)
-        
     
-    except FileNotFoundError:
-        print("\nFile does not exist.")
-        return(fileCheck())
 
-def menuSystem():
-
+while True:
     menuChoice = input("Welcome\nPlease type (1) to enter a word, (2) to enter a file name, or (0) to exit : ")
     if menuChoice in ["1", "2", "0"]:
         if menuChoice == "1":
-            sentence = sentenceCheck()
-            print(sentence)
+            sentenceCheck()
+                
 
         elif menuChoice == "2":
-            fileTest = fileCheck()
-            print(fileTest)
+            valChoice = False
+            while valChoice == False:
+                fNameCheck = input("\nPlease enter the filename that you would like to check, along with the .txt extension : ")
+                try:
+                    with open(fNameCheck, 'r') as file:
+                        
+                        fileList = file.read()
+                        wordOutput = fileList.split(" ")
+                        print(wordOutput)
+                        valChoice = True
+                except FileNotFoundError:
+                    print("\nFile does not exist.")
+                    valInput = False
+                    while valInput == False:
+                        choice = input("\nWould you like to :\n\nRe-enter the filename (1)\n\nReturn to main menu (2) ")
+                        if choice == "1":
+                            valInput = True
+                        elif choice == "2":
+                            valInput = True
+                            valChoice = True
+                        else:
+                            print("\nInvalid input\nPlease enter either 1 or 2.")   
 
         elif menuChoice == "0":
             print("Exiting program ... ")
             time.sleep(2)
-            os._exit
-    
+            break
+        
     else:
         print("\nPlease only enter either 1, 2 or 3")
-        return menuSystem()
-menuSystem()
