3ef0aef8e055e3ed3ede5630f887b9cde6107250
u17296as <ali.sarwar@student.manchester.ac.uk>
2020-11-30 18:54:57 +0000

    Created button objects and added to canvas

--- coursework_02/menu.py
+++ coursework_02/menu.py
@@ -18,6 +18,22 @@ bgLabel = Label(canvas, image=bgImage)
 bgLabel.pack()
 
 
+playButton = Button(mainMenu, text="Play Game", bg='#e0b522', fg='white', height=2, width=20, command= gameWindow)
+playButton.place(x=547.5, y=310)
 
+leaderButton = Button(mainMenu, text="Leaderboards", bg='#e0b522', fg='white', height=2, width=20, command= leaderWindow)
+leaderButton.place(x=547.5, y=360)
+
+controlsButton = Button(mainMenu, text="Change Controls", bg='#e0b522', fg='white', height=2, width=20, command= controlsWindow)
+controlsButton.place(x=547.5, y=410)
+
+loadButton = Button(mainMenu, text="Load Game", bg='#e0b522', fg='white', height=2, width=20, command= loadGame)
+loadButton.place(x=547.5, y=460)
+
+insButton = Button(mainMenu, text="How to Play", bg='#e0b522', fg='white', height=2, width=20, command= instructionWindow)
+insButton.place(x=547.5, y=510)
+
+exitButton = Button(mainMenu, text="Exit", bg='#e0b522', fg='white', height=2, width=20, command= exitProgram)
+exitButton.place(x=547.5, y=560)
 
 mainMenu.mainloop()
