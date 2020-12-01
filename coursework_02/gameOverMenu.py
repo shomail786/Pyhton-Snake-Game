5916793520f831e7195e81224103f03ad4491a9a
u17296as <ali.sarwar@student.manchester.ac.uk>
2020-12-01 16:02:12 +0000

    Creates and blaces buttons on the tkinter window for main menu, restart, leaderboard and exit

--- coursework_02/gameOverMenu.py
+++ coursework_02/gameOverMenu.py
@@ -18,7 +18,17 @@ bgLabel = Label(canvas, image=bgImage)
 bgLabel.pack()
 
 
+mainMenuButton = Button(gameOverMenu, text="Main Menu", bg='black', fg='white', height=4, width=20, command= menuWindow)
+mainMenuButton.place(x=105, y=115)
 
+restartButton = Button(gameOverMenu, text="Restart", bg='black', fg='white', height=4, width=20, command= restartProgram)
+restartButton.place(x=105, y=215)
+
+leaderButton = Button(gameOverMenu, text="Leaderboards", bg='black', fg='white', height=4, width=20, command= leaderWindow)
+leaderButton.place(x=105, y=315)
+
+exitButton = Button(gameOverMenu, text="Exit", bg='black', fg='white', height=4, width=20, command= exitProgram)
+exitButton.place(x=105, y=415)
 
 
 
